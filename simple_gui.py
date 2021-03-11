b8409230a2d24c17a8dbdcbf97ac0b71ea649ca8
Steven <16929888+stevenkyritsis@users.noreply.github.com>
2021-03-11 18:02:27 -0500

    Create simple_gui.py

---
+++ simple_gui.py
@@ -0,0 +1,12 @@
+#Creating a simple GUI interface
+
+import tkinter as tk
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+    
+    def create_widgets()
