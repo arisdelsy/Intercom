0ac63d1c7867806fab40a862772dfcbf99059de5
arisdelsy <62040441+arisdelsy@users.noreply.github.com>
2021-03-24 10:37:00 -0400

    Update doorbell.py

--- doorbell.py
+++ doorbell.py
@@ -40,7 +40,7 @@ import uuid
 #import selenium
 
 from selenium import webdriver
-#from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait 
